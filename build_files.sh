5e1b0c3fdb93959b608a8845b0bac46864170283
nldq092203 <le_diem_quynh.nguyen@insa-cvl.fr>
2024-06-19 17:00:08 +0200

    .

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,9 @@
 
-python -m venv env
-.\env\Scripts\activate 
-.\env\Scripts\python.exe -m pip install --upgrade pip
+python3 -m venv env
+./env/Scripts/activate 
+./env/Scripts/python.exe -m pip install --upgrade pip
 
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
