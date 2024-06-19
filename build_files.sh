74fd62a632d1b7d63896088aa0b4184c88c88d04
nldq092203 <le_diem_quynh.nguyen@insa-cvl.fr>
2024-06-19 16:10:04 +0200

    Use requirements.txt

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-set -e
 
-# Install pipenv if it's not installed
-pip install pipenv
-
-# Install dependencies from Pipfile.lock
-pipenv install --deploy --ignore-pipfile
-
-# Run database migrations
-pipenv run python manage.py migrate
-
-# Collect static files
-pipenv run python manage.py collectstatic --noinput
+pip install -r requirements.txt
+python3.10 manage.py collectstatic --noinput
